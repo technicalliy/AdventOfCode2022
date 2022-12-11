572b174282d951cba62d5289bed537790578fdfb
Alliy Frauenpreis <alliyfrauenpreis@gmail.com>
2022-12-11 13:45:11 -0800

    Days 4 and 5

---
+++ Day 4/cleanup.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+let input = fs.readFileSync('cleanup.txt').toString().split("\n");
+
+let containCount = 0;
+
+input.forEach((pair) => {
+
+	const pairArray = pair.split(',');
+	const firstRange = pairArray[0].split('-').map(item => parseInt(item))
+	const secondRange = pairArray[1].split('-').map(item => parseInt(item))
+
+	if ((firstRange[0] <= secondRange[0]) && (firstRange[1] >= secondRange[1])) {
+		containCount += 1;
+	} else if ((secondRange[0] <= firstRange[0]) && (secondRange[1] >= firstRange[1])) {
+		containCount += 1;
+	}
+})
+
+console.log(containCount)
---
+++ Day 5/supply.js
@@ -0,0 +1,92 @@
+const fs = require('fs')
+let input = fs.readFileSync('supply.txt').toString().split("\n");
+
+// You could have more than 9 stacks but let's leave it like this for now
+let stacks = [
+	{ stack: 1, items: [] },
+	{ stack: 2, items: [] },
+	{ stack: 3, items: [] },
+	{ stack: 4, items: [] },
+	{ stack: 5, items: [] },
+	{ stack: 6, items: [] },
+	{ stack: 7, items: [] },
+	{ stack: 8, items: [] },
+	{ stack: 9, items: [] },
+];
+
+let lines = [];
+
+// Remove duplicate spaces
+for (let i = 0; i < input.length; i++) {
+
+	let thisLine = input[i].split(" ");
+	if (thisLine.includes("1")) break;
+
+	// It's ugly but every 4 '' === one empty stack placement
+	for (let j = 0; j < thisLine.length; j++) {
+		if (thisLine[j] === '' && thisLine[j+1] === '' && thisLine[j+2] === '' && thisLine[j+3] === '') {
+			thisLine.splice(j, 3)
+		}
+	}
+
+	lines.push(thisLine)
+}
+
+for (let i = 0; i < lines.length; i++) {
+
+	let thisLine = lines[i]
+
+	for (let j = 0; j < thisLine.length; j++) {
+
+		let currStackItem = thisLine[j];
+		if (thisLine[j] !== '') {
+
+			// Put item in existing stack
+			let currStack = stacks.find(x => x.stack == j + 1);
+			if (currStack != undefined) {
+				currStack.items.push(thisLine[j])
+			} 
+		}
+	}
+}
+
+
+// Parse out our rules
+const instructions = input.filter(line => line.includes("from"))
+let rules = [];
+
+instructions.forEach(instruction => {
+	let thisLineRules = instruction.split(" ")
+	thisLineRules = thisLineRules.filter(rule => !isNaN(rule))
+	rules.push(thisLineRules)
+})
+
+// Apply the rules
+rules.forEach(rule => {
+
+	const numItems = parseInt(rule[0])
+	const fromStack = parseInt(rule[1])
+	const toStack = parseInt(rule[2])
+
+	let holdItems = [];
+
+	// Remove numItems from fromStack
+	for (let i = 0; i < numItems; i++) {
+		let removedItem = stacks.filter(currStack => currStack.stack === fromStack)[0].items.shift();
+		holdItems.push(removedItem)
+	}
+
+	// Add holdItems to toStack
+	for (let i = 0; i < holdItems.length; i++) {
+		stacks.filter(currStack => currStack.stack === toStack)[0].items.unshift(holdItems[i]);
+	}
+})
+
+
+// Get letters of each top of stack
+let stackString = '';
+stacks.forEach(stack => {
+	if (stack.items.length) stackString += stack.items[0].replace('[', '').replace(']','');
+})
+
+console.log(stackString)
